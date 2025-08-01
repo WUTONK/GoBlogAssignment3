f7a1b6ab52b0b6def0d24288cf7ce653c21d1857
WUTONK <wjt474696120@gmail.com>
2025-08-01 15:26:47 +0800

    修复api地址问题

--- frontend/src/shared/index.ts
+++ frontend/src/shared/index.ts
@@ -1,5 +1,5 @@
 import { DefaultApi,Configuration } from "../api";
 
 export const Api = new DefaultApi(new Configuration({
-    basePath: "http://localhost:8080"
+    basePath: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080"
 }))
